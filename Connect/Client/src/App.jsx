22481fe7c8ad4edcb642353851176de2c77c5498
Rishabh <iamrishabhnirmalkar@gmail.com>
2023-12-19 18:53:34 +0530

    connection

---
+++ Connect/Client/src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [jokes, setjokes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/jokes")
+      .then((response) => {
+        setjokes(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+  return (
+    <>
+      <h1>This is full stack</h1>
+      <p>JOKES: {jokes.length}</p>
+
+      {jokes.map((joke, index) => (
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default App;
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Backend
- 
+## Backend
+
+# Connect
+
+1. "type": "module",
+2. Bundler
+3. axios
+4. proxy
